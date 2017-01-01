36c7cea28c963fc7eb283d7187e1b1beb060bc05
Manuel Rodríguez Vicente <manuelrodriguezvicnt@hotmail.com>
2017-01-01 14:25:20 +0100

    Add files via upload

---
+++ Programa/Librerias/src/minikame.h
@@ -0,0 +1,48 @@
+#ifndef minikame_h
+#define minikame_h
+
+#include <Servo.h>
+#include <Octosnake.h>
+
+class MiniKame{
+
+    public:
+        void init();
+        void run(float steps, float period);
+        void walk(float steps, float period);
+        void omniWalk(float steps, float T, bool side, float turn_factor);
+        //void backward(float steps, float period);
+        void turnL(float steps, float period);
+        void turnR(float steps, float period);
+        void moonwalkL(float steps, float period);
+        void dance(float steps, float period);
+        void upDown(float steps, float period);
+        void pushUp(float steps, float period);
+        void hello();
+        void jump();
+        void home();
+        void zero();
+        void frontBack(float steps, float period);
+
+        void setServo(int id, float target);
+        void reverseServo(int id);
+        float getServo(int id);
+        void moveServos(int time, float target[8]);
+
+    private:
+        Oscillator oscillator[8];
+        Servo servo[8];
+        int board_pins[8];
+        int trim[8];
+        bool reverse[8];
+        unsigned long _init_time;
+        unsigned long _final_time;
+        unsigned long _partial_time;
+        float _increment[8];
+        float _servo_position[8];
+
+        //int angToUsec(float value);
+        void execute(float steps, float period[8], int amplitude[8], int offset[8], int phase[8]);
+};
+
+#endif
---
+++ Programa/programa_kame/programa_kame.ino
@@ -0,0 +1,101 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include "minikame.h"
+#include <Octosnake.h>
+
+MiniKame robot;
+
+bool running = 0;
+
+int orden = 0;
+int movimiento = 100;
+
+void setup() {
+
+  robot.init();
+  Serial.begin(9600);
+
+ // delay(500);
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+
+    orden = Serial.read();
+    Serial.print("I received: ");
+    Serial.println(orden, DEC);
+  }
+  // este else es potencialmente comentable. es para que pare si no recibe nada por serial
+  // dicho esto se modificara en funcion de como responda desde el movil
+  else{
+    orden = 0;
+  }
+
+// nada que decir, es el switch principal, cada numero se corresponde a una letra de teclado en codigo ascii que es lo que recive por bluethooth.
+
+  switch (orden) {
+
+     case 119: //w---alante
+      robot.run(4,430);
+      running = 1;
+      break;
+
+     case 97:  //a---izquierda
+      robot.turnL(1, 430);
+      running = 1;
+      break;
+
+     case 100: //d---derecha
+      robot.turnR(1, 430);
+      running = 1;
+      break;
+
+     case 115: //s---stop
+      //STOP
+      running = 0;
+      break;
+
+     case 102: //f---flexiones
+      robot.pushUp(2, 500);
+      break;
+
+     case 99: //c---cangrejo (saltitos)
+      robot.upDown(4, 200);
+      break;
+
+     case 106: //j---salto
+      robot.jump();
+      break;
+
+     case 104: //h---hola
+      robot.hello();
+      break;
+
+     case 109: //m---moonWalkl
+      robot.moonwalkL(4,1000);
+     
+    //  robot.omniWalk(2,1000,true,5);
+    //  frontBack();
+    
+      break;
+
+     case 98: //b---baile
+      robot.dance(2, 700);
+      break;
+
+case 48: //0---invertir servos
+robot.reverseServo(2);
+robot.reverseServo(3);
+robot.reverseServo(6);
+robot.reverseServo(7);
+delay (250);
+      break;
+      
+
+    default:  //posicion inicial
+      robot.zero();
+      break;
+  }
+}
+
